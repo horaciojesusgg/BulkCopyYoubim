ea8ff1f40e2b6f93204ca931ad9c74ee9e7d49c0
Horacio <horaciojesusgg@gmail.com>
2019-06-20 15:57:59 -0430

    initial commit

---
+++ ClassLibraryYouBim/BulkCopy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ClassLibraryYouBim
+{
+    public static class BulkCopy
+    {
+        public static void BulKCopyXml(string xmlFilePath, string databaseName, string tableName, string serverName)
+        {
+            if (!File.Exists(xmlFilePath))
+            {
+                Console.WriteLine("The file does not exist.");
+                return;
+            }
+
+            string cs = "server=" + serverName + ";database=" + databaseName + ";integrated security=true";
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                DataSet dataset = new DataSet();
+
+                dataset.ReadXml(xmlFilePath);
+
+                DataTable table = dataset.Tables[tableName];
+
+                con.Open();
+
+                using (SqlBulkCopy sb = new SqlBulkCopy(con))
+                {
+                    sb.DestinationTableName = tableName;
+                    foreach (DataColumn column in table.Columns)
+                    {
+                        sb.ColumnMappings.Add(column.ColumnName, column.ColumnName);
+                    }
+                    sb.WriteToServer(table);
+                }
+            }
+        }
+    }
+}
---
+++ TestYoubimLibrary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ClassLibraryYouBim;
+namespace TestYoubimLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indicate the full path of the xml file:");
+            string xmlPath = Console.ReadLine();
+
+            Console.WriteLine("Indicate the database name");
+            string dbName = Console.ReadLine();
+
+            Console.WriteLine("Indicate the table name");
+            string tableName = Console.ReadLine();
+
+            Console.WriteLine("Indicate the server name");
+            string serverName = Console.ReadLine();
+
+            BulkCopy.BulKCopyXml(xmlPath, dbName, tableName, serverName);
+            Console.WriteLine("Process end.");
+        }
+    }
+}
